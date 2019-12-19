7b5b01f958acdeef8bee362f311acc3092b28d05
Kwsk114514 <15054@hakodate.kosen-ac.jp>
2019-12-19 09:16:27 +0900

    HPbarの挙動を更新

--- SouthwestProject/HPbar.java
+++ SouthwestProject/HPbar.java
@@ -12,6 +12,7 @@ public class HPbar extends Actor
     GreenfootImage HP_img = new GreenfootImage("HP.png");
     GreenfootImage ReducedHP_img = new GreenfootImage("reducedHP.png");
     private int barWidth = 1200, barHeight = 20;
+    private double current_HPrate = 1.0, goal_rate;
     /**
      * Act - do whatever the HPbar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -22,10 +23,21 @@ public class HPbar extends Actor
         HP_img.scale(barWidth, barHeight);
         ReducedHP_img.scale(barWidth, barHeight);
     } 
+    public void act()
+    {
+        if(goal_rate < current_HPrate)
+        {
+            current_HPrate -= (current_HPrate-goal_rate)*0.1;
+            CurrentHP_img.drawImage(HP_img, (int)((double)barWidth*(1.0-current_HPrate)), 0);
+            CurrentHP_img.drawImage(ReducedHP_img, (int)((double)barWidth*(-current_HPrate)), 0);
+            setImage(CurrentHP_img);
+        }
+    }
     public void setHP(double rate)
     {
-        CurrentHP_img.drawImage(HP_img, (int)((double)barWidth*(1.0-rate)), 0);
-        CurrentHP_img.drawImage(ReducedHP_img, (int)((double)barWidth*(-rate)), 0);
+        CurrentHP_img.drawImage(HP_img, (int)((double)barWidth*(1.0-current_HPrate)), 0);
+        CurrentHP_img.drawImage(ReducedHP_img, (int)((double)barWidth*(-current_HPrate)), 0);
         setImage(CurrentHP_img);
+        goal_rate = rate;
     }
 }
